0a0d61dd3919991807f7cf7adff9f8ca68e3b871
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2026-02-06 13:09:22 +0100

    feat: create qr widget

--- src/lib/form/components/qr-widget.svelte
+++ src/lib/form/components/qr-widget.svelte
@@ -1,8 +1,51 @@
 <script lang="ts">
 	import type { ComponentProps } from '@sjsf/form';
 
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	let { value = $bindable(), config, handlers }: ComponentProps['textWidget'] = $props();
+	import { TriangleAlert } from '@lucide/svelte';
+	import TextWidget from '@sjsf/shadcn4-theme/widgets/text.svelte';
+	import { getExceptionMessage } from '$lib/utils';
+	import qrcode from 'qrcode-generator';
+
+	//
+
+	// eslint-disable-next-line svelte/no-unused-svelte-ignore
+	// svelte-ignore custom_element_props_identifier
+	let { value = $bindable(), ...rest }: ComponentProps['textWidget'] = $props();
+
+	function generateQrCode(text: string, cellSize = 20) {
+		const qr = qrcode(0, 'L');
+		qr.addData(text);
+		qr.make();
+		return qr.createDataURL(cellSize);
+	}
+
+	const result = $derived.by(() => {
+		if (!value) return undefined;
+		try {
+			return generateQrCode(value);
+		} catch (e) {
+			return new Error(getExceptionMessage(e));
+		}
+	});
 </script>
 
-<p>GINOOOOOOO</p>
+<div class="flex gap-4">
+	<TextWidget bind:value {...rest} />
+	<div
+		class={[
+			'aspect-square size-48 shrink-0 overflow-hidden rounded-md border bg-slate-50',
+			'flex flex-col items-center justify-center gap-1',
+			'text-center text-xs text-muted-foreground',
+			(!result || result instanceof Error) && 'p-3'
+		]}
+	>
+		{#if result instanceof Error}
+			<TriangleAlert size={20} />
+			<p>{result.message}</p>
+		{:else if result}
+			<img src={result} class="aspect-square h-full w-full object-contain" alt="QR Code" />
+		{:else}
+			<p>Type something to generate a QR Code</p>
+		{/if}
+	</div>
+</div>
---
+++ src/lib/utils/index.ts
@@ -0,0 +1,6 @@
+export function getExceptionMessage(error: unknown): string {
+	if (error instanceof Error) {
+		return error.message;
+	}
+	return String(error);
+}
